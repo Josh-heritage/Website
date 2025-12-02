3a74e3ed02311d1489033a8da991c2a3325413fc
Josh-heritage <joshjacksonheritage@pm.me>
2025-12-02 18:12:37 +0000

    changed met office request again

--- atmosphere.js
+++ atmosphere.js
@@ -1,5 +1,9 @@
-fetch("https://metoffice-proxy.joshjacksonheritage.workers.dev//functions/metoffice")
-  .then(r => r.json())
+fetch("https://metoffice-proxy.joshjacksonheritage.workers.dev")
+  .then(resp => {
+    if (!resp.ok) throw new Error("Network response was not ok");
+    return resp.json();
+  })
   .then(data => {
     console.log(data);
-  });
+  })
+  .catch(err => console.error("Error fetching forecast:", err));
